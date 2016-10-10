21d05ec5c0929f12d51647d4eb4f3342adcd6d85
davidande <david.ande@wanadoo.fr>
2016-10-10 13:23:23 +0200

    Add files via upload

---
+++ 2008.ps1
@@ -0,0 +1,108 @@
+﻿# 2008.ps1
+# https://github.com/nexxai/CryptoBlocker
+
+################################ Functions ################################
+
+function ConvertFrom-Json20([Object] $obj)
+{
+    Add-Type -AssemblyName System.Web.Extensions
+    $serializer = New-Object System.Web.Script.Serialization.JavaScriptSerializer
+    return ,$serializer.DeserializeObject($obj)
+}
+
+Function New-CBArraySplit {
+
+    param(
+        $extArr,
+        $depth = 1
+    )
+
+    $extArr = $extArr | Sort-Object -Unique
+
+    # Concatenate the input array
+    $conStr = $extArr -join ','
+    $outArr = @()
+
+    # If the input string breaks the 4Kb limit
+    If ($conStr.Length -gt 4096) {
+        # Pull the first 4096 characters and split on comma
+        $conArr = $conStr.SubString(0,4096).Split(',')
+        # Find index of the last guaranteed complete item of the split array in the input array
+        $endIndex = [array]::IndexOf($extArr,$conArr[-2])
+        # Build shorter array up to that indexNumber and add to output array
+        $shortArr = $extArr[0..$endIndex]
+        $outArr += [psobject] @{
+            index = $depth
+            array = $shortArr
+        }
+
+        # Then call this function again to split further
+        $newArr = $extArr[($endindex + 1)..($extArr.Count -1)]
+        $outArr += New-CBArraySplit $newArr -depth ($depth + 1)
+        
+        return $outArr
+    }
+    # If the concat string is less than 4096 characters already, just return the input array
+    Else {
+        return [psobject] @{
+            index = $depth
+            array = $extArr
+        }  
+    }
+}
+
+################################ Functions ################################
+
+# Add to all drives
+$drivesContainingShares = Get-WmiObject Win32_Share | Select Name,Path,Type | Where-Object { $_.Type -eq 0 } | Select -ExpandProperty Path | % { "$((Get-Item -ErrorAction SilentlyContinue $_).Root)" } | Select -Unique
+if ($drivesContainingShares -eq $null -or $drivesContainingShares.Length -eq 0)
+{
+    Write-Host "No drives containing shares were found. Exiting.."
+    exit
+}
+
+Write-Host "The following shares needing to be protected: $($drivesContainingShares -Join ",")"
+
+
+$fileGroupName = "ALTAE_Crypto_extensions"
+$fileTemplateName = "ALTAE_Template_Crypto"
+$fileScreenName = "ALTAE_Filtre_Crypto"
+
+$webClient = New-Object System.Net.WebClient
+$jsonStr = $webClient.DownloadString("https://fsrm.experiant.ca/api/v1/get")
+$monitoredExtensions = @(ConvertFrom-Json20($jsonStr) | % { $_.filters })
+
+# Split the $monitoredExtensions array into fileGroups of less than 4kb to allow processing by filescrn.exe
+$fileGroups = New-CBArraySplit $monitoredExtensions
+ForEach ($group in $fileGroups) {
+    $group | Add-Member -MemberType NoteProperty -Name fileGroupName -Value "$FileGroupName$($group.index)"
+}
+
+# Perform these steps for each of the 4KB limit split fileGroups
+ForEach ($group in $fileGroups) {
+    Write-Host "Adding/replacing File Group [$($group.fileGroupName)] with monitored file [$($group.array -Join ",")].."
+    &filescrn.exe filegroup Delete "/Filegroup:$($group.fileGroupName)" /Quiet
+    &filescrn.exe Filegroup Add "/Filegroup:$($group.fileGroupName)" "/Members:$($group.array -Join '|')"
+}
+
+Write-Host "Adding/replacing File Screen Template [$fileTemplateName] with Event Notification [notification.cfg].."
+&filescrn.exe Template Delete /Template:$fileTemplateName /Quiet
+rm notification.cfg
+New-Item notification.cfg -type file
+Add-Content notification.cfg "Notification=e"
+Add-Content notification.cfg "`nRunLimitInterval=30"
+Add-Content notification.cfg "`nMessage=User [Source Io Owner] attempted to save [Source File Path] to [File Screen Path] on the [Server] server. This file is in the [Violated File Group] file group. This file could be a marker for malware infection, and should be investigated immediately."
+# Build the argument list with all required fileGroups
+$screenArgs = 'Template','Add',"/Template:$fileTemplateName"
+ForEach ($group in $fileGroups) {
+    $screenArgs += "/Add-Filegroup:$($group.fileGroupName)"
+}
+
+&filescrn.exe $screenArgs /Add-Notification:"e,notification.cfg"
+
+Write-Host "Adding/replacing File Screens.."
+$drivesContainingShares | % {
+    Write-Host "`tAdding/replacing File Screen for [$_] with Source Template [$fileTemplateName].."
+    &filescrn.exe Screen Delete "/Path:$_" /Quiet
+    &filescrn.exe Screen Add "/Path:$_" "/SourceTemplate:$fileTemplateName"
+}
