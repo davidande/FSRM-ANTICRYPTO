eee14257fcfff17c6fa548f5d3720e5fe93556b0
David Andé <david.ande@wanadoo.fr>
2016-12-23 16:35:29 +0100

    Update FSRM_NoCrypto_2008.ps1
    add drive and extension exception

--- FSRM_NoCrypto_2008.ps1
+++ FSRM_NoCrypto_2008.ps1
@@ -20,6 +20,22 @@
 ########## VARIABLE TO MODIFY #############
 $wkdir = "C:\FSRMNOCRYPTO"
 ###########################################
+# Drives to exclude for FSRM bloking
+# If You want to exclude complete Path or special directory so write for exemple "C:\share" for  specific share 
+# or "D:\shar*" for all shares in D starting by shar or "E:\*" for all shares in E
+# or"D:\*shar*"for all shares in D containing shar.
+# If nothing to exclude let the value to "0". only one value per line so only 2 exclusions for the moment :-)
+# ex: $drive_exclu1= "C:\Windows*"
+$drive_exclu1= "0"
+$drive_exclu2= "0"
+#############################################
+
+# Extensions to exclude from bloking list
+# Same as drive exclusion
+# ex: $ext_exclu1 = "*.777"
+$ext_exclu1 = "0"
+$ext_exclu2 = "0"
+#############################################
 
 # verifying if new crypto extensions available #
 $url = "https://fsrm.experiant.ca/api/v1/get"
@@ -90,6 +106,27 @@ if ($drivesContainingShares -eq $null -or $drivesContainingShares.Length -eq 0)
     Write-Host "No drives containing shares were found. Exiting.."
     exit
 }
+$drivesContainingShares >> "$wkdir\drivesbase.txt"
+if ($drive_exclu2 -ne '0' ) {
+    $drives_filter = (Get-Content .\drivesbase.txt | where { $_ -notlike "$drive_exclu1"} | where { $_ -notlike "$drivee_xclu2"})
+    $drivesContainingShares = $drivesfilter}
+    Else {
+    if ($drive_exclu1 -ne '0') {
+    $drivesfilter = (Get-Content .\drivesbase.txt | where { $_ -notlike "$drive_exclu1"})
+    $drivesContainingShares = $drivesfilter}
+    Else {
+    }
+    }
+Write-Host "Shared filtered"
+if ($drivesContainingShares -eq $null -or $drivesContainingShares.Length -eq 0)
+{
+    Write-Host "No drives containing shares were found. Exiting.."
+	cp $wkdir\extensions.txt $wkdir\extensions.old
+	rm $wkdir\drivesbase.txt
+	rm $wkdir\extensions.txt
+echo finish
+    exit
+}
 
 Write-Host "The following shares needing to be protected: $($drivesContainingShares -Join ",")"
 
@@ -101,6 +138,19 @@ $fileScreenName = "ALTAE_CryptoBlockerScreen"
 $webClient = New-Object System.Net.WebClient
 $jsonStr = $webClient.DownloadString($url)
 $monitoredExtensions = @(ConvertFrom-Json20($jsonStr) | % { $_.filters })
+$monitoredExtensions >> "$wkdir\extsbase.txt"
+
+if ($ext_exclu2 -ne '0') {
+    $ext_filter = (Get-Content "$wkdir\extsbase.txt" | where { $_ -notlike "$ext_exclu1"} | where { $_ -notlike "$ext_exclu2"})
+    $monitoredExtensions = $ext_filter }
+    Else {
+    if ($ext_exclu1 -ne '0') {
+    $ext_filter = (Get-Content "$wkdir\extsbase.txt" | where { $_ -notlike "$ext_exclu1"})
+    $monitoredExtensions = $ext_filter}
+    Else {
+    }
+    }
+Write-Host "Extension accepted $ext_exclu1,$ext_exclu2"
 
 # Split the $monitoredExtensions array into fileGroups of less than 4kb to allow processing by filescrn.exe
 $fileGroups = New-CBArraySplit $monitoredExtensions
@@ -146,6 +196,8 @@ if (Test-Path "$wkdir\extension.old")
 Else  
 {
 cp $wkdir\extensions.txt $wkdir\extensions.old
+rm $wkdir\drivesbase.txt
+rm $wkdir\extsbase.txt
 rm $wkdir\extensions.txt
 echo finish
 }
